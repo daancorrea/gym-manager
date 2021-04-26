6b2fcbfd5fe9ee31906b146295380dcd2393a1ad
Daniel Corrêa <daniel.123.correa@gmail.com>
2021-04-26 20:45:26 -0300

    Adições
    Adicionei uma pagina para edição de formulário já cadastrado.

--- instructors.js
+++ instructors.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
+const {age} = require('./utils')
 
 //show
 exports.show = function (req, res) {
@@ -14,14 +15,14 @@ exports.show = function (req, res) {
 
     const instructor = {
         ... foundInstructor,
-        age:"",
-        gender:"",
+        age: age(foundInstructor.birth),
         services:foundInstructor.services.split(","),
-        created_at:"",
+        created_at: new Intl.DateTimeFormat("pt-br").format(foundInstructor.created_at),
     }
 
     return res.render("instructors/show", {instructor})
 }
+
 
 //create
 exports.post = function (req, res) {    
@@ -48,7 +49,7 @@ exports.post = function (req, res) {
             name,
             birth,
             gender,
-            services
+            services,
             created_at
         })
 
@@ -60,3 +61,18 @@ exports.post = function (req, res) {
 })
 }
 
+//edit
+
+exports.edit = function(req, res) {
+
+    const {id} = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+    if (!foundInstructor) {
+        return res.send("Instructor not found!!")
+    }
+
+    return res.render('instructors/edit',{instructor:foundInstructor})
+}
--- routes.js
+++ routes.js
@@ -14,6 +14,8 @@ routes.get('/instructors/create',function(req, res) {
     return res.render('instructors/create')
 })
 
+routes.get('/instructors/:id/edit', instructors.edit)
+
 routes.get('/instructors/:id', instructors.show)
 
 routes.post('/instructors', instructors.post)
