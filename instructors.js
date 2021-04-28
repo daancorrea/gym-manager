aff5ef231c5a12bd29b6366175bee41b1f94817c
Daniel Corrêa <daniel.123.correa@gmail.com>
2021-04-28 06:47:06 -0300

    new inclusions
    Fiz o caminho do botão "Editar".
    Arrumei a data do aniversário para ser alterada.
    Criado um put para salvar as informações atualizadas no `data.json`

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const {age} = require('./utils')
+const {age, date} = require('./utils')
 
 //show
 exports.show = function (req, res) {
@@ -73,6 +73,39 @@ exports.edit = function(req, res) {
     if (!foundInstructor) {
         return res.send("Instructor not found!!")
     }
+    const instructor = {
+        ... foundInstructor,
+        birth: Date(foundInstructor.birth)
+    }
+    return res.render('instructors/edit',{instructor})
+}
 
-    return res.render('instructors/edit',{instructor:foundInstructor})
+//put
+exports.put = function(req,res) {
+    const {id} = req.body
+    let index = 0
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex) {
+        if (id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
+    if (!foundInstructor) {
+        return res.send("Instructor not found!!")
+    }
+
+    const instructor = {
+        ... foundInstructor,
+        ... req.body,
+        birth: Date.parse(req.body.birth)
+    }
+    
+    date.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(date, null, 2), function(error){
+        if(error) return res.send("Write error!!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+
 }
--- routes.js
+++ routes.js
@@ -19,6 +19,8 @@ routes.get('/instructors/:id/edit', instructors.edit)
 routes.get('/instructors/:id', instructors.show)
 
 routes.post('/instructors', instructors.post)
+
+routes.put("/instructors",instructors.put)
     
 routes.get('/members', function(req, res){
     return res.send("members")
